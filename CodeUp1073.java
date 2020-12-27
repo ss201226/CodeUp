f3a1e9d7d3d5329f6e8471a220c8517a508692ab
ss201226 <75982925+ss201226@users.noreply.github.com>
2020-12-27 22:59:03 +0900

    Add files via upload

---
+++ CodeUp1073.java
@@ -0,0 +1,27 @@
+package basic;
+
+import java.util.Scanner;
+
+public class CodeUp1073 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();;
+		while(true) {
+			if(n!=0) {
+				 System.out.println(n);
+				 n=sc.nextInt();
+				
+				 if(n==0) {
+					 break;
+				 }
+
+	
+		}
+		}
+	}
+}
+
+
+
